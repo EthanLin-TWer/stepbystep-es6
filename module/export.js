6c7d7e741a2722fc46a107bc2f6f3deabce82ef7
linesh <linesh.simpcity@gmail.com>
2016-11-21 21:39:50 +0800

    [#13] Linesh: Use namespace export and default export of class

--- module/export.js
+++ module/export.js
@@ -4,3 +4,12 @@ export const namedObject = {
    key: 'isvalue',
    value: 'iskey'
 }
+export default class {
+   constructor(name = 'Michael', age = 21) {
+      this.name = name
+      this.age = age
+   }
+
+   getName() { return this.name }
+   getAge() { return this.age }
+}
--- module/import.js
+++ module/import.js
@@ -4,3 +4,17 @@ console.log('------------Named Imports/Exports------------')
 console.log('number constant: ' + namedConstant)
 console.log('string constant: ' + namedString)
 console.log('object constant: ' + namedObject.key + ', ' + namedObject.value)
+
+import * as named from './export'
+
+console.log('------------Namespace Imports/Exports------------')
+console.log('number constant: ' + named.namedConstant)
+console.log('string constant: ' + named.namedString)
+console.log('object constant: ' + named.namedObject.key + ', ' + namedObject.value)
+
+import Person from './export'
+
+console.log('------------Default Imports/Exports------------')
+let jennifer = new Person('Jennifer', 25);
+console.log('name: ' + jennifer.getName())
+console.log('age: ' + jennifer.getAge())
